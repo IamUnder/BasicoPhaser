a378cb308c81ccf67f5ddca2fba70497b4f5469b
Under <jorgeolmo.I@gmail.com>
2021-03-12 20:50:39 +0100

    Distribución por carpetas

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+import Nivel1 from './scene/nivel1';
+
+const config = {
+    type: Phaser.AUTO,
+    backgroundColor: '#125555',
+    width: 800,
+    height: 600,
+    scene: Nivel1
+};
+
+export default config;
